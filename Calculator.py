05ee7c189fc928d91afe4d0c93014dc215ccff14
Hadrien RENAUD <hadrien.renaud@polytechnique.edu>
2019-06-11 16:02:17 +0200

    add handles custom delimiters

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,16 @@
+import re
 
-from re import split
 
 class Calculator:
 
     @staticmethod
     def add(inputs: str):
+        delimiters=r'[,\n]'
         if len(inputs) == 0:
             return 0
-        inputs_list = split(r'[,\n]', inputs)
+        if not re.match(r'[.\d-]', inputs.split('\n')[0]):
+            delimiters = inputs.split('\n')[0]
+            inputs = "\n".join(inputs.split('\n')[1:])
+        inputs_list = re.split(delimiters, inputs)
         return sum(map(float, inputs_list))
+
